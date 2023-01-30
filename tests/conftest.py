1a72a45f5916beeea0e178bc9b0e3e13ac48862e
Agustin Marquez <agusdmb@gmail.com>
2023-01-30 21:47:03 +0100

    Refactor conftest, add function factory

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,32 @@
+from collections import namedtuple
+
 import pytest
 
 from inspector import Inspector, Mode
+
+Functions = namedtuple(
+    "Functions", ["simplest_func", "constant_func", "args_func", "args_kwargs_func"]
+)
+
+
+def functions_factory(inspector: Inspector) -> Functions:
+    @inspector("simplest_func")
+    def simplest_func():
+        return None
+
+    @inspector("constant_func")
+    def constant_func():
+        return 1
+
+    @inspector("args_func")
+    def args_func(a, b):
+        return a + b
+
+    @inspector("args_kwargs_func")
+    def args_kwargs_func(a, b):
+        return a + b
+
+    return Functions(simplest_func, constant_func, args_func, args_kwargs_func)
 
 
 @pytest.fixture(scope="function", name="inspector")
@@ -10,35 +36,23 @@ def fixture_inspector():
 
 @pytest.fixture(name="simplest_func")
 def fixture_simplest_func(inspector):
-    @inspector("simplest_func")
-    def function():
-        pass
-
-    return function
+    functions = functions_factory(inspector)
+    return functions.simplest_func
 
 
 @pytest.fixture(name="constant_func")
 def fixture_constant_func(inspector):
-    @inspector("constant_func")
-    def function():
-        return 1
-
-    return function
+    functions = functions_factory(inspector)
+    return functions.constant_func
 
 
 @pytest.fixture(name="args_func")
 def fixture_args_func(inspector):
-    @inspector("args_func")
-    def function(a, b):
-        return a + b
-
-    return function
+    functions = functions_factory(inspector)
+    return functions.args_func
 
 
 @pytest.fixture(name="args_kwargs_func")
 def fixture_args_kwargs_func(inspector):
-    @inspector("args_kwargs_func")
-    def function(a, b):
-        return a + b
-
-    return function
+    functions = functions_factory(inspector)
+    return functions.args_kwargs_func
