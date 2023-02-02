5c950e47dc9d9975025c971a7d9088610ac24469
Agustin Marquez <agusdmb@gmail.com>
2023-02-02 23:53:55 +0100

    add test

--- tests/test_backend.py
+++ tests/test_backend.py
@@ -27,6 +27,18 @@ class TestJsonBackend:
             )
             assert json_backend.get_result("function", 3, b=4) == 7
 
+    def test_json_backend_get_multi(self):
+        with tempfile.NamedTemporaryFile() as temp:
+            json_backend = JSONBackend(temp.name)
+            json_backend.record_call(
+                "function", Record(args=(3,), kwargs={"b": 4}, result=7)
+            )
+            json_backend.record_call(
+                "function", Record(args=(9,), kwargs={"b": 11}, result=20)
+            )
+            assert json_backend.get_result("function", 3, b=4) == 7
+            assert json_backend.get_result("function", 9, b=11) == 20
+
     def test_json_backend_no_value(self):
         with tempfile.NamedTemporaryFile() as temp:
             json_backend = JSONBackend(temp.name)
